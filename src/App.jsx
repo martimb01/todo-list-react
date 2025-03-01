3fa4dd64a9a4ab4fdbb6fbfd8a0ca7f67a3a410f
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-01 16:44:25 +0000

    Todo list working

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,28 @@ export default function App() {
         false},
       ]
     })
+    setNewItem('')
  }
+
+ function toggleTodo(id, completed) {
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if (todo.id === id) {
+        return {...todo, completed}
+      }
+
+      return todo
+    })
+  })
+ }
+
+ function deleteTodo(id) {
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id !== id)
+  })
+ }
+
+ console.log(todos);
 
   return (
     <>
@@ -27,20 +48,17 @@ export default function App() {
   </form>
   <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
-      <label>
-        <input type="checkbox"></input>
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-    <li>
-      <label>
-        <input type="checkbox"></input>
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo =>{
+      return <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed}
+                 onChange={e => {toggleTodo(todo.id, e.target.checked)}}></input>
+                {todo.title}
+              </label>
+              <button onClick={() => {deleteTodo(todo.id)}} className="btn btn-danger">Delete</button>
+            </li>
+    })}
   </ul>
   </>
   )
